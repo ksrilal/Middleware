4a4486fbf1664be9422a94642664701b9a40989f
ksrilal <ksrilalranasinghe95@gmail.com>
2020-08-16 12:22:55 +0530

    Spring Boot frontend api small changes

--- stums/src/main/java/com/middleware/stums/models/UserDTO.java
+++ stums/src/main/java/com/middleware/stums/models/UserDTO.java
@@ -1,35 +1,17 @@
 package com.middleware.stums.models;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-
-@Entity
 public class UserDTO {
 
-    @Id
-    @GeneratedValue(strategy=GenerationType.AUTO)
-    @Column(name = "user_id")
     private Integer id;
-
-    @Column(name  = "firstName")
     private String firstName;
-
-    @Column(name  = "lastName")
     private String lastName;
-
-    @Column(name  = "age")
     private Integer age;
-
-    @Column(name  = "email")
     private String email;
+
 
     public Integer getId() {
         return id;
     }
-
     public void setId(Integer id) {
         this.id = id;
     }
@@ -37,7 +19,6 @@ public class UserDTO {
     public String getFirstName() {
         return firstName;
     }
-
     public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
@@ -45,7 +26,6 @@ public class UserDTO {
     public String getLastName() {
         return lastName;
     }
-
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
@@ -53,7 +33,6 @@ public class UserDTO {
     public Integer getAge() {
         return age;
     }
-
     public void setAge(Integer age) {
         this.age = age;
     }
@@ -61,7 +40,6 @@ public class UserDTO {
     public String getEmail() {
         return email;
     }
-
     public void setEmail(String email) {
         this.email = email;
     }
